c2a699f967e13544d551ae434c7b3e5ae09cbb6b
kishan <kishan@tyfone.com>
2018-09-21 15:15:41 +0530

    Updated front page style

--- csc.js
+++ csc.js
@@ -5,13 +5,12 @@ let InfoOutput = {
 	log: function() {
 		let line = Array.prototype.slice.call(arguments).map(function(argument) {
 			return typeof argument === 'string' ? argument : JSON.stringify(argument);
-		}).join(' ');
-		
-		document.querySelector('#log').textContent += line + '\n';
+		}).join(' ');		
+		document.querySelector('#txtArea').textContent += line + '\n';
 	},
 	
 	clearLog: function() {
-		document.querySelector('#log').textContent = '';
+		document.querySelector('#txtArea').textContent = '';
 	},
 };
 clearLog = InfoOutput.clearLog;
